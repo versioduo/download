c64ed2f01430b9bf279af18dd607885c47cd6609
Kay Sievers <kay@versioduo.com>
2025-09-22 23:56:19 +0200

    Really fix SERCOM

--- build-core.sh
+++ build-core.sh
@@ -19,8 +19,8 @@ git clone --depth=1 git@github.com:${vendor}/arduino-board-package
 (cd arduino-board-package && git submodule update --init --recursive --filter=blob:none)
 
 # Revert SERCOM
-sed -i '' -E 's/sercom->I2CM.STATUS.bit.BUSERR/sercom->I2CM.INTFLAG.bit.ERROR/' arduino-board-package/ArduinoCore-samd/cores/arduino/SERCOM.cpp
-grep -q 'sercom->I2CM.INTFLAG.bit.ERROR' arduino-board-package/ArduinoCore-samd/cores/arduino/SERCOM.cpp
+sed -i '' -E 's/sercom->I2CM.INTFLAG.bit.ERROR/sercom->I2CM.STATUS.bit.BUSERR/' arduino-board-package/ArduinoCore-samd/cores/arduino/SERCOM.cpp
+grep -q 'sercom->I2CM.STATUS.bit.BUSERR' arduino-board-package/ArduinoCore-samd/cores/arduino/SERCOM.cpp
 
 # Increase the number of USB descriptor strings.
 sed -i '' -E 's/STRING_DESCRIPTOR_MAX = 12/STRING_DESCRIPTOR_MAX = 24/' arduino-board-package/ArduinoCore-samd/libraries/Adafruit_TinyUSB_Arduino/src/arduino/Adafruit_USBD_Device.h
